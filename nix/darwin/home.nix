90496d9f56aa6f92708ea154bb68ed88209d4327
bright owl <121698803+thinksolve@users.noreply.github.com>
2025-12-10 18:14:12 -0500

    cleaned up home.nix

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -9,6 +9,16 @@ let
   dotfiles_dir = "${config.home.homeDirectory}/.dotfiles";
   link_dotfiles = path: config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/${path}";
 
+  # maps repettive files and diretory structures
+  outOfStoreLinks =
+    targetDir: sourceDir: entries:
+    builtins.listToAttrs (
+      map (entry: {
+        name = "${targetDir}/${entry}";
+        value.source = config.lib.file.mkOutOfStoreSymlink "${sourceDir}/${entry}";
+      }) entries
+    );
+
   # Helper function: 'pkg.meta.broken' is overridden to allow broken package
   unbreak =
     pkg:
@@ -83,15 +93,7 @@ in
 {
   home.username = "brightowl";
   home.homeDirectory = "/Users/brightowl";
-  # home.username = lib.mkForce username;
-  # home.homeDirectory = lib.mkForce "/Users/${username}";
 
-  # home.username = username;
-  # home.homeDirectory = "/Users/${username}";  <-- commented out oct 2025
-
-  # home.homeDirectory = "/Users/${config.home.username}";
-  # home.homeDirectory = lib.mkForce "/Users/brightowl";
-  # home.stateVersion = "24.11";
   home.stateVersion = "23.11";
 
   home.packages =
@@ -104,56 +106,52 @@ in
   #      > $HOME/.zshrc_flat_test
   # '';
 
-  home.file = {
-    ".local/bin/sg" = {
-      text = ''
-        #!/usr/bin/env bash
-        exec ${pkgs.ast-grep}/bin/ast-grep "$@"
-      '';
-      executable = true;
+  home.file =
+    (outOfStoreLinks ".config/zsh" "${dotfiles_dir}/zsh" [
+      "after_compinit.zsh"
+      "aliases.zsh"
+      "bindkeys.zsh"
+      "constants.zsh"
+      "fast_compinit.zsh"
+      "path.sh"
+      "preferences.zsh"
+      "terminal_styling.zsh"
+    ])
+    // (outOfStoreLinks ".local/bin" "${dotfiles_dir}/bin" [
+      "recent"
+      "nvim"
+      "yt-txt"
+    ])
+    // (outOfStoreLinks ".config/" "${dotfiles_dir}/" [
+      "doom"
+      "ghostty"
+      "iterm2"
+      "nix"
+      "nvim"
+      "yazi"
+    ])
+    // {
+      ".config/nix-darwin".source = link_dotfiles "/nix/darwin";
+      ".hammerspoon".source = link_dotfiles "/hammerspoon";
+      ".shell_functions.sh".source = link_dotfiles "/zsh/.shell_functions.sh";
+      ".tmux.conf".source = link_dotfiles "/tmux/.tmux.conf";
+      ".zsh_plugins.txt".source = link_dotfiles "/zsh/.zsh_plugins.txt";
+      ".zshrc".source = link_dotfiles "/zsh/.zshrc";
+      "translate-romance-languages".source = link_dotfiles "/misc_projects/translate-romance-languages";
+      # ".doom.d/init.el".source = link_dotfiles "/doom/init.el";
+      # ".doom.d/config.el".source = link_dotfiles "/doom/config.el";
+      # ".doom.d/packages.el".source = link_dotfiles "/doom/packages.el";
+      ".local/bin/sg" = {
+        text = ''
+          #!/usr/bin/env bash
+          exec ${pkgs.ast-grep}/bin/ast-grep "$@"
+        '';
+        executable = true;
+      };
+      "/Library/Application Support/com.mitchellh.ghostty/config".source =
+        link_dotfiles "/ghostty/config";
+
     };
-    "/Library/Application Support/com.mitchellh.ghostty/config".source =
-      link_dotfiles "/ghostty/config";
-
-    ".config/ghostty".source = link_dotfiles "/ghostty/";
-
-    ".local/bin/recent".source = link_dotfiles "/bin/recent";
-
-    ".local/bin/nvim".source = link_dotfiles "/bin/nvim-recent";
-    # ".local/bin/nvim" = {
-    #   source = link_dotfiles "/bin/nvim-recent";
-    #   executable = true;
-    # };
-
-    ".local/bin/yt-txt".source = link_dotfiles "/bin/yt-txt";
-    # ".local/bin/yt-txt" = {
-    #   source = link_dotfiles "/bin/yt-txt";
-    #   executable = true;
-    # };
-    ".hammerspoon".source = link_dotfiles "/hammerspoon";
-    ".config/path.sh".source = link_dotfiles "/zsh/path.sh";
-    ".config/zsh/aliases.zsh".source = link_dotfiles "/zsh/aliases.zsh";
-    ".config/zsh/after_compinit.zsh".source = link_dotfiles "/zsh/after_compinit.zsh";
-    ".config/zsh/bindkeys.zsh".source = link_dotfiles "/zsh/bindkeys.zsh";
-    ".config/zsh/constants.zsh".source = link_dotfiles "/zsh/constants.zsh";
-    ".config/zsh/preferences.zsh".source = link_dotfiles "/zsh/preferences.zsh";
-    ".config/zsh/fast_compinit.zsh".source = link_dotfiles "/zsh/fast_compinit.zsh";
-    ".config/zsh/terminal_styling.zsh".source = link_dotfiles "/zsh/terminal_styling.zsh";
-    ".zshrc".source = link_dotfiles "/zsh/.zshrc";
-    ".zsh_plugins.txt".source = link_dotfiles "/zsh/.zsh_plugins.txt";
-    ".tmux.conf".source = link_dotfiles "/tmux/.tmux.conf";
-    ".shell_functions.sh".source = link_dotfiles "/zsh/.shell_functions.sh";
-    ".config/nvim".source = link_dotfiles "/nvim";
-    ".config/iterm2".source = link_dotfiles "/iterm2";
-    ".config/yazi".source = link_dotfiles "/yazi";
-    ".config/nix".source = link_dotfiles "/nix";
-    ".config/nix-darwin".source = link_dotfiles "/nix/darwin";
-    ".config/doom".source = link_dotfiles "/doom";
-    "translate-romance-languages".source = link_dotfiles "/misc_projects/translate-romance-languages";
-    # ".doom.d/init.el".source = link_dotfiles "/doom/init.el";
-    # ".doom.d/config.el".source = link_dotfiles "/doom/config.el";
-    # ".doom.d/packages.el".source = link_dotfiles "/doom/packages.el";
-  };
   home.activation.zcompile = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ${pkgs.zsh}/bin/zsh -c '
       [[ -f $HOME/.zshrc ]] && zcompile -Uz $HOME/.zshrc
