56995489b7a832d2e7750720472bb7c0cbf79b1d
bright owl <121698803+thinksolve@users.noreply.github.com>
2025-12-12 01:28:11 -0500

    cleaned up home.nix some mo

--- nix/darwin/home.nix
+++ nix/darwin/home.nix
@@ -7,16 +7,17 @@
 }:
 let
   dotfiles_dir = "${config.home.homeDirectory}/.dotfiles";
-  link_dotfiles = path: config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/${path}";
+  dotfiles = path: config.lib.file.mkOutOfStoreSymlink "${dotfiles_dir}/${path}";
 
-  # maps repettive files and diretory structures
+  # Makes bearable symlinking many (but not all) items with identical basenames (generally relative paths!).
+  # e.g. "${symPrefix}/${basename}".source = config.lib.file.mkOutOfStoreSymlink "${targetPrefix}/${basename}"
   outOfStoreLinks =
-    targetDir: sourceDir: entries:
+    symPrefix: targetPrefix: relPaths:
     builtins.listToAttrs (
-      map (entry: {
-        name = "${targetDir}/${entry}";
-        value.source = config.lib.file.mkOutOfStoreSymlink "${sourceDir}/${entry}";
-      }) entries
+      map (relPath: {
+        name = "${symPrefix}/${relPath}";
+        value.source = config.lib.file.mkOutOfStoreSymlink "${targetPrefix}/${relPath}";
+      }) relPaths
     );
 
   # Helper function: 'pkg.meta.broken' is overridden to allow broken package
@@ -99,59 +100,38 @@ in
   home.packages =
     cli ++ dev_tools ++ file_system ++ pdf_and_document ++ terminal_and_shell_enhancements;
 
-  # home.activation.build-zshrc-flat = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-  #   cat \
-  #     ${./zsh/aliases} \
-  #     ${./zsh/exports} \
-  #      > $HOME/.zshrc_flat_test
-  # '';
-
-  home.file =
-    (outOfStoreLinks ".config/zsh" "${dotfiles_dir}/zsh" [
-      "after_compinit.zsh"
-      "aliases.zsh"
-      "bindkeys.zsh"
-      "constants.zsh"
-      "fast_compinit.zsh"
-      "path.sh"
-      "preferences.zsh"
-      "terminal_styling.zsh"
-    ])
-    // (outOfStoreLinks ".local/bin" "${dotfiles_dir}/bin" [
-      "recent"
-      "nvim"
-      "yt-txt"
-    ])
-    // (outOfStoreLinks ".config/" "${dotfiles_dir}/" [
-      "doom"
-      "ghostty"
-      "iterm2"
-      "nix"
-      "nvim"
-      "yazi"
-    ])
-    // {
-      ".config/nix-darwin".source = link_dotfiles "/nix/darwin";
-      ".hammerspoon".source = link_dotfiles "/hammerspoon";
-      ".shell_functions.sh".source = link_dotfiles "/zsh/.shell_functions.sh";
-      ".tmux.conf".source = link_dotfiles "/tmux/.tmux.conf";
-      ".zsh_plugins.txt".source = link_dotfiles "/zsh/.zsh_plugins.txt";
-      ".zshrc".source = link_dotfiles "/zsh/.zshrc";
-      "translate-romance-languages".source = link_dotfiles "/misc_projects/translate-romance-languages";
-      # ".doom.d/init.el".source = link_dotfiles "/doom/init.el";
-      # ".doom.d/config.el".source = link_dotfiles "/doom/config.el";
-      # ".doom.d/packages.el".source = link_dotfiles "/doom/packages.el";
-      ".local/bin/sg" = {
-        text = ''
-          #!/usr/bin/env bash
-          exec ${pkgs.ast-grep}/bin/ast-grep "$@"
-        '';
-        executable = true;
-      };
-      "/Library/Application Support/com.mitchellh.ghostty/config".source =
-        link_dotfiles "/ghostty/config";
-
-    };
+  home.file = {
+    ".config/nix-darwin".source = dotfiles "/nix/darwin";
+    ".hammerspoon".source = dotfiles "/hammerspoon";
+    ".shell_functions.sh".source = dotfiles "/zsh/.shell_functions.sh";
+    ".tmux.conf".source = dotfiles "/tmux/.tmux.conf";
+    ".zsh_plugins.txt".source = dotfiles "/zsh/.zsh_plugins.txt";
+    ".zshrc".source = dotfiles "/zsh/.zshrc";
+    "/Library/Application Support/com.mitchellh.ghostty/config".source = dotfiles "/ghostty/config";
+    # ".doom.d/init.el".source = dotfiles "/doom/init.el";
+    # ".doom.d/config.el".source = dotfiles "/doom/config.el";
+    # ".doom.d/packages.el".source = dotfiles "/doom/packages.el";
+    "translate-romance-languages".source = dotfiles "/misc_projects/translate-romance-languages";
+  }
+  // (outOfStoreLinks ".local/" "${dotfiles_dir}/" [
+    "bin/recent"
+    "bin/yt-txt"
+  ])
+  // (outOfStoreLinks ".config/" "${dotfiles_dir}/" [
+    "doom"
+    "ghostty"
+    "iterm2"
+    "nix"
+    "nvim"
+    "yazi"
+    "zsh/after_compinit.zsh"
+    "zsh/aliases.zsh"
+    "zsh/bindkeys.zsh"
+    "zsh/constants.zsh"
+    "zsh/fast_compinit.zsh"
+    "zsh/preferences.zsh"
+    "zsh/terminal_styling.zsh"
+  ]);
   home.activation.zcompile = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ${pkgs.zsh}/bin/zsh -c '
       [[ -f $HOME/.zshrc ]] && zcompile -Uz $HOME/.zshrc
